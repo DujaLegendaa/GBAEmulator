1e995c9f00197dd3d1e9c6a24ae4ef2e3a561233
anjachan <fontwinter@gmail.com>
2021-04-05 14:21:59 +0200

    base cpu

---
+++ src/cpu.rs
@@ -0,0 +1,28 @@
+struct Z80{
+    a: u8,
+    f: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8,
+    sp: u16,
+    pc: u16
+}
+impl Z80{
+    fn new() -> Self{
+        Self{
+            a: 0,
+            f: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            h: 0,
+            l: 0,
+            sp: 0,
+            pc: 0
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+mod cpu;
 fn main() {
     println!("Hello world!");
     println!("OU MAJ GAD DUSAN KUN :O");
